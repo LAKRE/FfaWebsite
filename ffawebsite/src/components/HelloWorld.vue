7dd35f62b0ec6f69cc156f7ec020aac2e1f0c14e
lakre <akre.lionel@gmail.com>
2019-01-13 16:15:30 +0000

    lets get started for real

---
+++ ffawebsite/src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div class="header">
+    </div>
+    <div class="main">
+    </div>  
+
+    <div class="footer">
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  .header {
+    height: 4.5em;
+    width: 100%;
+    background-color: black;
+  }
+
+  .main{
+    width:100%;
+    height:50em;
+    background-image: url("../../public/img/FfaNicourtCebanFilipponi.jpg");
+    background-position: center;
+    background-size: cover;
+  }
+</style>
